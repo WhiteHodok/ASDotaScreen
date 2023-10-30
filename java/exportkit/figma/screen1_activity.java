ec6fb7217dc0377988d7b383600de730cb3f5e14
Vladislav Mikhaylyuck <39564937+WhiteHodok@users.noreply.github.com>
2023-10-31 01:37:17 +0600

    Features-Stage1
    - С горем пополам пилится 1 фрейм приложения 
    - Очень сложно
    - Ничего не понятно

---
+++ java/exportkit/figma/screen1_activity.java
@@ -0,0 +1,88 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		screen1
+	 *	@date 		Monday 30th of October 2023 06:08:43 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class screen1_activity extends Activity {
+
+	
+	private View _bg__screen1_ek2;
+	private View _bg__mask_group_ek1;
+	private ImageView image_16;
+	private View rectangle_24;
+	private View rectangle_22;
+	private View _bg__group_107_ek1;
+	private View _bg__group_106_ek1;
+	private TextView dota_2_is_a_multiplayer_online_battle_arena__moba__game_which_has_two_teams_of_five_players_compete_to_collectively_destroy_a_large_structure_defended_by_the_opposing_team_known_as_the__ancient___whilst_defending_their_own_;
+	private View _bg__group_95_ek1;
+	private View _bg__group_88_ek1;
+	private View _bg__group_83_ek1;
+	private View _bg__group_82_ek1;
+	private View rectangle_25;
+	private View rectangle_27;
+	private View rectangle_26;
+	private ImageView image_17;
+	private View rectangle_31;
+	private View _bg__group_96_ek1;
+	private View rectangle_32;
+	private View rectangle_33;
+	private TextView install;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.screen1);
+
+		
+		_bg__screen1_ek2 = (View) findViewById(R.id._bg__screen1_ek2);
+		_bg__mask_group_ek1 = (View) findViewById(R.id._bg__mask_group_ek1);
+		image_16 = (ImageView) findViewById(R.id.image_16);
+		rectangle_24 = (View) findViewById(R.id.rectangle_24);
+		rectangle_22 = (View) findViewById(R.id.rectangle_22);
+		_bg__group_107_ek1 = (View) findViewById(R.id._bg__group_107_ek1);
+		_bg__group_106_ek1 = (View) findViewById(R.id._bg__group_106_ek1);
+		dota_2_is_a_multiplayer_online_battle_arena__moba__game_which_has_two_teams_of_five_players_compete_to_collectively_destroy_a_large_structure_defended_by_the_opposing_team_known_as_the__ancient___whilst_defending_their_own_ = (TextView) findViewById(R.id.dota_2_is_a_multiplayer_online_battle_arena__moba__game_which_has_two_teams_of_five_players_compete_to_collectively_destroy_a_large_structure_defended_by_the_opposing_team_known_as_the__ancient___whilst_defending_their_own_);
+		_bg__group_95_ek1 = (View) findViewById(R.id._bg__group_95_ek1);
+		_bg__group_88_ek1 = (View) findViewById(R.id._bg__group_88_ek1);
+		_bg__group_83_ek1 = (View) findViewById(R.id._bg__group_83_ek1);
+		_bg__group_82_ek1 = (View) findViewById(R.id._bg__group_82_ek1);
+		rectangle_25 = (View) findViewById(R.id.rectangle_25);
+		rectangle_27 = (View) findViewById(R.id.rectangle_27);
+		rectangle_26 = (View) findViewById(R.id.rectangle_26);
+		image_17 = (ImageView) findViewById(R.id.image_17);
+		rectangle_31 = (View) findViewById(R.id.rectangle_31);
+		_bg__group_96_ek1 = (View) findViewById(R.id._bg__group_96_ek1);
+		rectangle_32 = (View) findViewById(R.id.rectangle_32);
+		rectangle_33 = (View) findViewById(R.id.rectangle_33);
+		install = (TextView) findViewById(R.id.install);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
